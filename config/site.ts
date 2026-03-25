4201ed7c7a8b36d1af56f4eb85a39a5193b6fab5
kunalkeshan <kunalkeshan12@gmail.com>
2026-03-25 16:50:08 +0530

    feat: add environment configuration and site URL handling
    * create .env.example file to define the NEXT_PUBLIC_SITE_URL variable for environment setup
    * implement siteUrl and siteOrigin exports in site.ts to dynamically determine the site URL based on environment variables
    
    This update establishes a foundational configuration for managing the site's URL, enhancing flexibility for different deployment environments.

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteUrl: string =
+  process.env.NEXT_PUBLIC_SITE_URL ??
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000");
+
+export const siteOrigin: URL = new URL(siteUrl);
